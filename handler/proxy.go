e46cb08581c2205797c4ce9b7cb4ed5b7994c6b3
riqalter <riqalter@gmail.com>
2025-06-15 11:57:17 +0700

    make proxy handler
    Signed-off-by: riqalter <riqalter@gmail.com>

--- handler/proxy.go
+++ handler/proxy.go
@@ -13,4 +13,35 @@ func Proxy(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Missing 'url' query parameter", http.StatusBadRequest)
 		return
 	}
+
+	targetURL, err := url.Parse(target)
+	if err != nil || targetURL.Scheme == "" || targetURL.Host == ""{
+		http.Error(w, "Invalid 'url' query parameter", http.StatusBadRequest)
+		return
+	}
+
+	req, err := http.NewRequest(r.Method, target, r.Body)
+	if err != nil {
+		http.Error(w, "Failed to create request", http.StatusInternalServerError)
+		return
+	}
+	req.Header = r.Header.Clone()
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, "Failed to fetch target URL", http.StatusBadGateway)
+		return
+	}
+	defer resp.Body.Close()
+
+	copyHeader(w.Header(), resp.Header)
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, v := range src {
+		dst[k] = v
+	}
 }
